97700c3252b089aba2cbd683481893046c40c6a4
tychon <tychon4.2@googlemail.com>
2012-11-20 16:51:31 +0100

    new haskell code for fourier analysis (you'll need bindings to fftw)

---
+++ fftw/fourier.hs
@@ -0,0 +1,24 @@
+
+-- cabal install vector
+-- cabal install fft
+
+import System.IO
+import qualified Data.Vector as V
+import qualified Data.Vector.Storable as VS
+import qualified Data.Array as A
+import qualified Data.Array.CArray as CA
+import Math.FFT
+import Data.Complex
+
+main = do
+  let xs = [0,0.01..(8*pi)] :: [Double]
+      n = length xs
+      --xs' = map (\x -> (sin (x))+(cos (x))+4*(cos(3*x))+(sin (5*x))) xs
+      xs' = map (\x -> 4*(sin (x))+(sin (8*x))+(sin (20*x))) xs
+  putStrLn $ unlines $ map (show) xs'
+  let (xsptr, dataoffset, datalength) = VS.unsafeToForeignPtr $ VS.fromList xs'
+  xsarray <- CA.unsafeForeignPtrToCArray xsptr ((1::Int), n)
+  let ysarray = dftRC xsarray
+  hPutStrLn stderr $ formatCSV $ map (/ (fromIntegral n)) $ CA.elems ysarray
+
+formatCSV xs = unlines $ map (\x@(xreal :+ ximag) -> (show xreal)++","++(show ximag)) xs
---
+++ fftw/fourierplot.m
@@ -0,0 +1,29 @@
+
+printf("reading in.csv ...\n");
+A = csvread("in.csv");
+printf("plotting ...\n");
+plot (A, ".-;input;");
+print ("-dpng", "plot_input.png");
+
+
+printf("reading out.csv ...\n");
+A = csvread("out.csv");
+
+printf("plotting 0-10...\n");
+xstart = 0;
+xend = min(10, length(A(:,1))-1);
+X = (xstart : xend);
+plot (X, A(1:(xend-xstart)+1,1), ".-;real;", X, A(1:(xend-xstart)+1,2), ".-;imag;");
+print ("-dpng", "plot_fourier0-10.png");
+
+printf("plotting 0-20...\n");
+xstart = 0;
+xend = min(20, length(A(:,1))-1);
+X = (xstart : xend);
+plot (X, A(1:(xend-xstart)+1,1), ".-;real;", X, A(1:(xend-xstart)+1,2), ".-;imag;");
+print ("-dpng", "plot_fourier0-20.png");
+
+printf("plotting all...\n");
+plot (A(:,1), ".-;real;", A(:,2), ".-;imag;");
+print ("-dpng", "plot_fourierAll.png");
+
