29fd43f847c25400c6ed8e2f3c7eea6ac2762738
tychon <tychon4.2@googlemail.com>
2012-11-24 19:47:51 +0100

    added calculation for energy

--- numerical/main.hs
+++ numerical/main.hs
@@ -11,8 +11,8 @@ import Data.List.Split
 g = -9.81
 -- Andere Konstanten
 time = 60.0
-timestep = 0.01
-optFps = 25
+timestep = 0.001
+optFps = 1000
 
 -- Startbedingungen
 l1 = 1
@@ -36,6 +36,12 @@ fphi2 p2 phi1 phi1d phi2d phi2 = ((-1)*k5*l1*(cos (phi1-phi2))*phi1d + p2) / k4
 fp1 phi1 phi2 phi1d phi2d = (-1)*k5*l1*phi1d*phi2d*(sin (phi1-phi2))
                                - g*k2*(sin phi1)- l1*g*k3*(sin phi1)
 fp2 phi1 phi2 phi1d phi2d = k5*l1*phi1d*phi2d*(sin (phi2-phi2)) - g*k5*(sin phi2)
+
+-- energies
+t1 phi1' = (1/2) * phi1'^2 * k1
+v1 phi1  = (-g) * k2 * (cos phi1)
+t2 phi1 phi2 phi1' phi2' = (1/2)*l1*phi1'^2*k3+(1/2)*phi2'^2*k4+l1*phi1'*phi2'*k5*(cos (phi1-phi2))
+v2 phi1 phi2 = (-g) * l1 * k3 * (cos phi1) - g * k5 * (cos phi2)
 
 -- Data structure for Pendulum:
 data Pendulum = Pendulum {
@@ -91,5 +97,9 @@ step (Pendulum phi1 phi1d p1 p1d phi2 phi2d p2 p2d) timeStep time
         p1'  = p1 + p1d' * timeStep
         p2d' = fp2 phi1 phi2 phi1d phi2d
         p2'  = p2 + p2d' * timeStep
-    in  [phi1d, phi1, phi2d, phi2, p1d, p1, p2d, p2] : (step (Pendulum phi1' phi1d' p1' p1d' phi2' phi2d' p2' p2d') timeStep (time-timeStep))
+        kin1 = t1 phi1d
+        pot1 = v1 phi1
+        kin2 = t2 phi1 phi2 phi1d phi2d
+        pot2 = v2 phi1 phi2
+    in  [phi1d, phi1, phi2d, phi2, p1d, p1, p2d, p2, kin1, pot1, kin2, pot2, (kin1+kin2), (pot1+pot2)] : (step (Pendulum phi1' phi1d' p1' p1d' phi2' phi2d' p2' p2d') timeStep (time-timeStep))
 
