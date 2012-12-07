797b9d2188e3e4b30ffee35d94f5a14fa90f4aab
tychon <tychon4.2@googlemail.com>
2012-12-08 00:46:48 +0100

    replayer: putting csv data into unix dom socket is working yeay

--- network/replay.c
+++ network/replay.c
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <strings.h>
+#include <assert.h>
 
 #define BUFFERSIZES 1024
 
@@ -31,7 +32,7 @@ int main(int argc, char *argv[]) {
       i ++;
       samplerate = atoi(argv[i]);
     }
-    else if (strcmp("-d", argv[i]) == 0) deleteOldSocket = 1;
+    else if (strcmp("-d", argv[i]) == 0) deleteSocketAfterUse = 1;
     else fprintf(stderr, "Ignoring unknown argument: \"%s\"", argv[i]);
   }
   
@@ -68,7 +69,7 @@ int main(int argc, char *argv[]) {
   
   printf("Connection established.\n");
   
-  printf("Opening csv file \"%s\" ...", csvpath);
+  printf("Opening csv file \"%s\" ...\n", csvpath);
   FILE *csvf = fopen(csvpath, "r");
   if (! csvf) {
     perror("opening csv file");
@@ -78,40 +79,58 @@ int main(int argc, char *argv[]) {
   if (samplerate < 0) printf("Sending samples as fast as we can ...\n");
   else printf("Sendings a rate of %d samples per second ...\n", samplerate);
   
-  int outputpos = 0;
-  int outreadpos = 0;
-  int lineLength = -1; // indicates if there is a full line in outbuffer
-  char readbuffer[BUFFERSIZES]; // this buffer the read method writes to
-  char outbuffer[BUFFERSIZES]; // this is a buffer with the formatted output
+  int bufferReadPos = 0;
+  int bufferWritePos = 0;
+  int lineLength = -1; // indicates if there is a full line in outbuffer and its length
+  char buffer[BUFFERSIZES]; // this buffer the read method writes to
   for (;;) {
     if (lineLength >= 0) {
+      printf(">");
       // send data
-      if (write(sock, outbuffer[outputpos], sizeof(char)*lineLength) < 0) { // TODO this may be wrong
+      if (write(msgsock, buffer+bufferReadPos, lineLength) < 0) {
+        perror("sending data");
+        goto CLOSING_ERR;
+      }
+      bufferReadPos += lineLength;
+      if (write(msgsock, "\n", 1) < 0) {
         perror("sending data");
         goto CLOSING_ERR;
       }
     } else {
-      // read new data
-      //TODO
+      //// read new data
+      // shift data to beginning:
+      for (int i = bufferReadPos; i < BUFFERSIZES; i++) buffer[i-bufferReadPos] = buffer[i];
+      bufferWritePos -= bufferReadPos;
+      bufferReadPos = 0;
+      // read data from file
+      int rval = fread(buffer+bufferReadPos, sizeof(char), BUFFERSIZES-bufferWritePos, csvf);
+      printf("<");
+      if (rval < 0) {
+        perror("reading from csv file");
+        goto CLOSING_ERR;
+      } else if (rval == 0) {
+        printf("\nend of data.\n");
+        break;
+      }
+      bufferWritePos += rval;
+      assert(bufferWritePos <= BUFFERSIZES);
+    }
+    
+    // skipt trailing newlines
+    while (buffer[bufferReadPos] == '\n' && bufferReadPos < bufferWritePos) bufferReadPos ++;
+    // find next lineLength
+    lineLength = -1;
+    for (int i = bufferReadPos; i < bufferWritePos; i++) {
+      if (buffer[i] == '\n') {
+        lineLength = i - bufferReadPos;
+        break;
+      }
+    }
+    if (lineLength == 0) {
+      fprintf(stderr, "empty line\n");
+      goto CLOSING_ERR;
     }
   }
-  
-  /*
-  int rval;
-  int outputpos = 0;
-  int outreadpos = 0;
-  char readbuffer[BUFFERSIZES]; // this buffer the read method writes to
-  char outbuffer[BUFFERSIZES]; // this is a ringbuffer with the formatted output
-  while ( (rval = fread(readbuffer, sizeof(char), BUFFERSIZES, csvf)) > 0) {
-    for (int i = 0; i < rval; i++) {
-      readbuffer[i]
-    }
-    //TODO implement tuning of samplerate
-  }
-  if (rval < 0) {
-    perror("reading from csv");
-    goto CLOSING_ERR;
-  }*/
   
   printf("closing file ...\n");
   fclose(csvf);
