10dad8756754825c68510404e210a76570eb54f2
tychon <tychon4.2@googlemail.com>
2012-12-12 16:48:48 +0100

    testreader.c: support printing binary

--- network/testreader.c
+++ network/testreader.c
@@ -7,18 +7,41 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CMP(str, index) (strcmp(str, argv[index]) == 0)
+#define FORMATHEX 1
+#define FORMATDEC 2
+#define FORMATBIN 3
+#define FORMATASC 4
+
+#define BYTETOBINARYPATTERN "%d%d%d%d%d%d%d%d"
+#define BYTETOBINARY(byte)  \
+  (byte & 0x80 ? 1 : 0), \
+  (byte & 0x40 ? 1 : 0), \
+  (byte & 0x20 ? 1 : 0), \
+  (byte & 0x10 ? 1 : 0), \
+  (byte & 0x08 ? 1 : 0), \
+  (byte & 0x04 ? 1 : 0), \
+  (byte & 0x02 ? 1 : 0), \
+  (byte & 0x01 ? 1 : 0) 
+
+#define PRINTABLE(ch) (ch >= 32 && ch <= 126)
+
 int main(int argc, char *argv[]) {
   int sock;
   struct sockaddr_un server;
   
   char *sockpath = NULL;
-  int showcharacters = 0;
-  for (int i = 0; i < argc; i++) {
-    if (strcmp("-c", argv[i]) == 0) {
-      showcharacters = 1;
-    } else if (strcmp("-b", argv[i]) == 0) {
-      showcharacters = 2;
-    } else sockpath = argv[i];
+  int format = FORMATHEX;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp("--format", argv[i]) == 0 || strcmp("-f", argv[i]) == 0) {
+      i++;
+      if (CMP("bin", i) || CMP("binary", i)) format = FORMATBIN;
+      else if (CMP("dec", i) || CMP("decimal", i)) format = FORMATDEC;
+      else if (CMP("hex", i) || CMP("hexadecimal", i)) format = FORMATHEX;
+      else if (CMP("asc", i) || CMP("ascii", i)) format = FORMATASC;
+    }
+    else if (! sockpath) sockpath = argv[i];
+    else fprintf(stderr, "Argument ignored: %s\n", argv[i]);
   }
   if (! sockpath) {
     printf("usage: %s [-c | -b] <pathname>", argv[0]);
@@ -53,9 +76,16 @@ int main(int argc, char *argv[]) {
       printf("end of data\n");
       exit(0);
     }
-    if (showcharacters == 0) printf("%d ", (int)c);
-    else if (showcharacters == 1) printf("%c", c);
-    else if (showcharacters == 2) printf("%d (%c) ", (int)c, c);
+    switch (format) {
+      case FORMATBIN: {
+        if (PRINTABLE(c)) printf(BYTETOBINARYPATTERN"\t%c\n", BYTETOBINARY(c), c);
+        else printf(BYTETOBINARYPATTERN"\t%x\n", BYTETOBINARY(c), (int)c);
+        break;
+      }
+      case FORMATDEC: printf("%d ", (int)c); break;
+      case FORMATHEX: printf("%x ", (int)c); break;
+      case FORMATASC: printf("%c", c); break;
+    }
     fflush(stdout);
   }
   
