96198cc994cde62628463bf840fcc376ccc723a3
tychon <tychon4.2@googlemail.com>
2012-12-23 19:34:36 +0100

    projectreader: support reading radiuses and angles of pendulum data file

--- network/projectreader.c
+++ network/projectreader.c
@@ -8,23 +8,33 @@
 #include <string.h>
 
 #include "common.h"
+
 #include "projectreader.h"
 
+// buffer length for parsing keys and values
 #define BUFFERSIZE 1024
+
+// conditional for valid characters in keys and values
 #define ISVALID(c) (isalnum(c) || c == '.')
+
+// You pass these defines to 'readData' as mode argument.
+#define PENDULUM_DATA 1
+#define CALIBRATION_DATA 2
+#define ZERO_LEVEL_DATA 3
 
 #define DEBUG
 
 /**
- * The length of the line without comments or -1 if not valid characters
- * were found before a '#' character.
+ * @returns the length of the line without comments or -1 if not valid
+ * characters (by ISVALID macro) were found before a '#' character.
  */
 int findContent(char *line, int linelength) {
   int contfound = 0;
   for (int i = 0; i < linelength; i++) {
     if (ISVALID(line[i])) contfound = 1;
     else if (line[i] == '#') {
-      if (contfound) return i;
+      line[i] = '\n';
+      if (contfound) return i+1;
       else return -1;
     }
   }
@@ -34,9 +44,9 @@ int findContent(char *line, int linelength) {
 }
 
 /**
- * @returns length of valid string in 'buffer' or errorcode:
- * -1: end of line reached and no '=' was found
- * -2: output buffer too small
+ * @returns size of valid string in 'buffer' or errorcode:
+ *   -1: end of line reached and no '=' was found
+ *   -2: output buffer too small
  */
 int readToken(char *line, int linelength, char **endptr
              , char *buffer, int bufferlength
@@ -62,7 +72,13 @@ int readToken(char *line, int linelength, char **endptr
   return -1; // no terminating delimiter found
 }
 
-projectdata *readPendulumData(projectdata *dest, char *filepath) {
+/**
+ * Parse key-value-pairs in file given by 'filepath' and store them in 'dest'.
+ * The 'mode' gives wich pairs are to be found in the file, see the 3 defines
+ * PENDULUM_DATA, CALIBRATION_DATA and ZERO_LEVEL_DATA.
+ * @returns 'dest'
+ */
+projectdata *readData(projectdata *dest, const char *filepath, const int mode) {
   FILE *f = fopen(filepath, "r");
   if ( ! f) {
     perror("opening file");
@@ -80,6 +96,7 @@ projectdata *readPendulumData(projectdata *dest, char *filepath) {
   while ( (linelength = getline(&line, &dummy, f)) >= 0) {
     linenum ++;
     if ( (linelength = findContent(line, linelength)) < 0) {
+      // line without any valid character for a value
       continue;
     }
     
@@ -111,7 +128,11 @@ projectdata *readPendulumData(projectdata *dest, char *filepath) {
       printf("value (length %d): %s\n", vallength, valbuffer);
     #endif
     
-    //TODO
+    // compare keys and fill destination structure
+    
+    // this define compares a string to the keybuffer, then parses the
+    // double in the valuebuffer, if the comparison succeeded and saves the
+    // double in 'to'.
     #define CMPREAD(str, to) (strcmp(str, keybuffer) == 0) { \
           to = strtod(valbuffer, &endptr); \
           if (endptr == valbuffer) { \
@@ -119,13 +140,66 @@ projectdata *readPendulumData(projectdata *dest, char *filepath) {
             exit(1);                 \
           } \
         }
-    if CMPREAD("l1a", dest->l1a)
-    else if CMPREAD("l1b", dest->l1b)
-    else if CMPREAD("l1", dest->l1)
-    else if CMPREAD("l2a", dest->l2a)
-    else if CMPREAD("l2b", dest->l2b)
-    else if CMPREAD("l1m", dest->l1m)
-    else if CMPREAD("l2m", dest->l2m)
+    
+    if (mode == PENDULUM_DATA) {
+      // parse lengths of pendulum in doubles
+      if CMPREAD("l1a", dest->l1a)
+      else if CMPREAD("l1b", dest->l1b)
+      else if CMPREAD("l1", dest->l1)
+      else if CMPREAD("l2a", dest->l2a)
+      else if CMPREAD("l2b", dest->l2b)
+      else if CMPREAD("l1m", dest->l1m)
+      else if CMPREAD("l2m", dest->l2m)
+      // parse number of solenoids as integer
+      else if (strcmp("solnum", keybuffer) == 0) {
+        dest->solnum = atoi(valbuffer);
+        if (dest < 0) {
+          fprintf(stderr, "error: expected number of solenoids to be a natural number in line %d\n", linenum);
+          exit(1);
+        }
+        dest->sols = assert_malloc(sizeof(double) * dest->solnum);
+        for (int i = 0; i < dest->solnum; i++)
+          dest->sols[i] = assert_malloc(sizeof(double)*4);
+      }
+      // try to read solenoid radiuses and angles
+      else {
+        int solindex;
+        // radiuses
+        if (sscanf(keybuffer, "solr%d", &solindex) == 1) {
+          if (solindex < 0 && solindex >= dest->solnum) {
+            fprintf(stderr, "error: solenoid index not in range in line %d\n", linenum);
+            exit(1);
+          }
+          // parse double
+          dest->sols[solindex][0] = strtod(valbuffer, &endptr);
+          if (endptr == valbuffer) {
+            fprintf(stderr, "error: Invalid radius in line %d\n", linenum);
+            exit(1);
+          }
+        }
+        // angles
+        if (sscanf(keybuffer, "sola%d", &solindex) == 1) {
+          if (solindex < 0 && solindex >= dest->solnum) {
+            fprintf(stderr, "error: solenoid index not in range in line %d\n", linenum);
+            exit(1);
+          }
+          // parse double
+          dest->sols[solindex][1] = strtod(valbuffer, &endptr);
+          if (endptr == valbuffer) {
+            fprintf(stderr, "error: Invalid angle in line %d\n", linenum);
+            exit(1);
+          }
+        }
+      }
+    }
+    else if (mode == CALIBRATION_DATA) {
+      if CMPREAD("k1", dest->k1)
+      else if CMPREAD("k2", dest->k2)
+      else if CMPREAD("k3", dest->k3)
+      else if CMPREAD("k4", dest->k4)
+    }
+    //TODO read zero level data
+    
     #undef CMPREAD
     
     free(line);
@@ -137,5 +211,27 @@ projectdata *readPendulumData(projectdata *dest, char *filepath) {
   }
   
   return dest;
+}
+
+////////////////////
+// declared in header
+
+/**
+ * Parses the keys l1a, l1b, l1, l2a, l2b, l1m, l2m and the number of solenoids
+ * in solnum. Additionaly it parses the radiuses of the solenoids with the keys
+ * solrX and the angle of the solenoids with the keys solaX where X is the
+ * index of the solenoid going from 0 to solnum (excluded).
+ * NOTE: the solnum key must always occur before the radiuses and angles!
+ * 2. NOTE: A new array is initialized for 'sols' in 'dest', every time the
+ *   key-value-pair 'solnum' was parsed. Old content is not freed!
+ */
+projectdata *readPendulumData(projectdata *dest, const char *filepath) {
+  return readData(dest, filepath, PENDULUM_DATA);
+}
+projectdata *readCalibrationData(projectdata *dest, const char *filepath) {
+  return readData(dest, filepath, CALIBRATION_DATA);
+}
+projectdata *readZeroLevelData(projectdata *dest, const char *filepath) {
+  return readData(dest, filepath, ZERO_LEVEL_DATA);
 }
 
--- network/projectreader.h
+++ network/projectreader.h
@@ -3,28 +3,30 @@
 #define _PROJECTREADER_H
 
 struct projectdata {
+  // use 'readPendulumData' for these values
   // lengths of pendulum
   double l1a, l1b, l1, l2a, l2b;
   // positions of magnets
   double l1m, l2m;
   
   // constants of differentials
+  // these are read with 'readCalibrationData'
   double k1, k2, k3, k4;
   
   // solenoids
   int solnum; // number of solenoids
   // array of solenoids (with length 'solnum'), where the second array index is:
-  // 0: radius
-  // 1: angle in radians
-  // 3: zero level
-  // 4: normalization factor
-  double *sols[4];
+  // 0: radius (read by 'readPendulumData')
+  // 1: angle in radians (read by 'readPendulumData')
+  // 3: zero level (read by 'readZeroLevelData')
+  // 4: normalization factor (read by 'readZeroLevelData')
+  double **sols;
 };
 typedef struct projectdata projectdata;
 
-projectdata *readPendulumData(projectdata*, char *filepath);
-projectdata *readCalibrationData(projectdata*, char *filepath);
-projectdata *readZeroLevelData(projectdata*, char *filepath);
+projectdata *readPendulumData(projectdata*, const char *filepath);
+projectdata *readCalibrationData(projectdata*, const char *filepath);
+projectdata *readZeroLevelData(projectdata*, const char *filepath);
 
 #endif // _PROJECTREADER_H
 
