f3c03a9ec034c3c04e4721875b394e908cb0ab4f
tychon <tychon4.2@googlemail.com>
2012-12-23 17:22:07 +0100

    projectreader: read pendulum data

--- network/projectreader.c
+++ network/projectreader.c
@@ -5,11 +5,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
+#include "common.h"
 #include "projectreader.h"
 
 #define BUFFERSIZE 1024
-#define ISVALID(c) isalnum(c)
+#define ISVALID(c) (isalnum(c) || c == '.')
+
+#define DEBUG
 
 /**
  * The length of the line without comments or -1 if not valid characters
@@ -38,19 +42,19 @@ int readToken(char *line, int linelength, char **endptr
              , char *buffer, int bufferlength
              , char delimiter) {
   int bpos = 0;
-  for (*endptr = line; *endptr < line+linelength; *endptr ++) {
+  for (*endptr = line; *endptr < line+linelength; (*endptr) ++) {
     char c = **endptr;
     
     if (isblank(c)) {
       // skip
-    } else if (isalnum(c)) {
+    } else if (ISVALID(c)) {
       // character in token found
       buffer[bpos++] = c;
       if (bpos == bufferlength-1) return -2;
     } else if (c == delimiter) {
       // end of token reached
       buffer[bpos] = '\0'; // write terminating nullbyte
-      *endptr ++; // set endptr to first char after end of token
+      (*endptr) ++; // set endptr to first char after end of token
       return bpos;
     }
   }
@@ -65,22 +69,71 @@ projectdata *readPendulumData(projectdata *dest, char *filepath) {
     exit(1);
   }
   
-  char **line
-     , **endptr
-     , buffer[BUFFERSIZE];
-  int linelength, keylength;
-  *line = NULL;
-  while ( (linelength = getline(line, NULL, f)) >= 0) {
-    if ( (linelength = findContent(*line, linelength)) < 0)
+  char *line
+     , *endptr
+     , keybuffer[BUFFERSIZE]
+     , valbuffer[BUFFERSIZE];
+  int linelength, keylength, vallength;
+  size_t dummy = sizeof(line); // required for getline
+  line = NULL;
+  int linenum = 0;
+  while ( (linelength = getline(&line, &dummy, f)) >= 0) {
+    linenum ++;
+    if ( (linelength = findContent(line, linelength)) < 0) {
       continue;
+    }
     
     // read key
-    keylength = readToken(*line, linelength, endptr, buffer, BUFFERSIZE, '=');
-    printf("key: %.*s\n", keylength, buffer);
+    keylength = readToken(line, linelength, &endptr, keybuffer, BUFFERSIZE, '=');
+    if (keylength == -1) {
+      fprintf(stderr, "error: no '=' found in line %d\n", linenum);
+      exit(1);
+    }
+    if (keylength == -2) {
+      fprintf(stderr, "error: buffer overflow in line %d\n", linenum);
+      exit(1);
+    }
+    #ifdef DEBUG
+      printf("key   (length %d): %s\n", keylength, keybuffer);
+    #endif
+    
+    // read value
+    vallength = readToken(endptr, line+linelength-endptr, &endptr, valbuffer, BUFFERSIZE, '\n');
+    if (vallength == -1) {
+      fprintf(stderr, "error: no newline found in line %d\n", linenum);
+      exit(1);
+    }
+    if (vallength == -2) {
+      fprintf(stderr, "error: buffer overflow in line %d\n", linenum);
+      exit(1);
+    }
+    #ifdef DEBUG
+      printf("value (length %d): %s\n", vallength, valbuffer);
+    #endif
+    
     //TODO
+    #define CMPREAD(str, to) (strcmp(str, keybuffer) == 0) { \
+          to = strtod(valbuffer, &endptr); \
+          if (endptr == valbuffer) { \
+            fprintf(stderr, "error: Invalid value in line %d\n", linenum); \
+            exit(1);                 \
+          } \
+        }
+    if CMPREAD("l1a", dest->l1a)
+    else if CMPREAD("l1b", dest->l1b)
+    else if CMPREAD("l1", dest->l1)
+    else if CMPREAD("l2a", dest->l2a)
+    else if CMPREAD("l2b", dest->l2b)
+    else if CMPREAD("l1m", dest->l1m)
+    else if CMPREAD("l2m", dest->l2m)
+    #undef CMPREAD
     
     free(line);
-    *line = NULL;
+    line = NULL;
+  }
+  if ( ! feof(f) ) {
+    perror("reading file");
+    exit(1);
   }
   
   return dest;
--- network/tracker.c
+++ network/tracker.c
@@ -6,26 +6,39 @@
 #include <stdlib.h>
 
 #include "common.h"
+#include "projectreader.h"
 #include "uds_client.h"
 #include "protocol.h"
 
 int main(int argc, char *argv[]) {
   char *socketpath = NULL;
+  char *pendulumdatapath = NULL;
   
   for (int i = 1; i < argc; i++) {
-    socketpath = argv[i];
+    if (ARGCMP("--pendulum", i)) {
+      i ++;
+      pendulumdatapath = argv[i];
+    }
+    else {
+      socketpath = argv[i];
+    }
   }
-  if (! socketpath) {
+  
+  if (! socketpath) { //TODO
     printf("usage: %s SOCKETPATHNAME\n", argv[0]);
     exit(1);
   }
+  
+  fprintf(stderr, "reading pendulum data from \"%s\" ...\n", pendulumdatapath);
+  projectdata *pd = assert_malloc(sizeof(projectdata));
+  readPendulumData(pd, pendulumdatapath);
   
   printf("opening connection to server on \"%s\"\n", socketpath);
   udsclientsocket *udscs = uds_create_client(socketpath);
   
   // TODO
   
-  printf("end of data\n");
+  fprintf(stderr, "end of data\n");
   uds_close_client(udscs);
 }
 
