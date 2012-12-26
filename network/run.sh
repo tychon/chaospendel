85cca0b6d75b3e1c9766c2215d4f53c346a9856a
tychon <tychon4.2@googlemail.com>
2012-12-26 23:36:12 +0100

    run.sh: what if we tried more?

--- network/run.sh
+++ network/run.sh
@@ -21,9 +21,9 @@ if [ "$1" == "tracking" ]; then
   if [ "$2" == "replay" ]; then
     # start replayer in background
     echo -e "\nstarting replayer ...\n\n"
-    ./replay.x -f -d -nt -i data_values.csv -p data_pendulum -o rawsocket -r 1000 > /dev/null &
+    ./replay.x -f -d -nt -i data_values.csv -p data_pendulum -o rawsocket -r 100000 > /dev/null &
     
-    sleep 0.001
+    sleep 0.000000001
     echo -e "\nstarting tracker ...\n\n"
     ./tracker.x -p data_pendulum -n data_normalisation -i rawsocket -o anglessocket --printtemp > data_tracking_temporary.csv
     
