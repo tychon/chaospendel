d9be6bfdc416cc9de7e0cdfe652439b394098431
tychon <tychon4.2@googlemail.com>
2012-12-26 14:40:13 +0100

    tracker: write some test data to terminal

--- network/tracker.c
+++ network/tracker.c
@@ -2,16 +2,18 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "common.h"
 #include "projectreader.h"
+#include "uds_server.h"
 #include "uds_client.h"
 #include "protocol.h"
 
 int main(int argc, char *argv[]) {
-  char *socketpath = NULL;
+  char *socketpath = NULL, *outsockpath = NULL;
   char *pendulumdatapath = NULL;
   char *normalisationdatapath = NULL;
   
@@ -19,13 +21,14 @@ int main(int argc, char *argv[]) {
     if (argcmpass("--pendulum|-p", argc, argv, &i, &pendulumdatapath)) ;
     else if (argcmpass("--normalisation|-n", argc, argv, &i, &normalisationdatapath)) ;
     else if (argcmpass("--inputsocket|-i", argc, argv, &i, &socketpath)) ;
+    else if (argcmpass("--outputsocket|-o", argc, argv, &i, &outsockpath)) ;
     else {
       fprintf(stderr, "Unknown argument ignored: \"%s\"\n", argv[i]);
     }
   }
   
-  if (! socketpath || ! pendulumdatapath || ! normalisationdatapath) {
-    printf("usage: %s --pendulum|-p PATH --normalisation|-n PATH --inputsocket|-i PATH\n", argv[0]);
+  if (! socketpath || ! pendulumdatapath || ! normalisationdatapath || ! outsockpath) {
+    printf("usage: %s --pendulum|-p PATH --normalisation|-n PATH --inputsocket|-i PATH --outputsocket|-o PATH\n", argv[0]);
     exit(1);
   }
   
@@ -37,8 +40,12 @@ int main(int argc, char *argv[]) {
   fprintf(stderr, "reading normalisation data from \"%s\" ...\n", normalisationdatapath);
   readNormalisationData(pd, normalisationdatapath);
   
-  printf("opening connection to server on \"%s\"\n", socketpath);
+  fprintf(stderr, "opening connection to server on \"%s\"\n", socketpath);
   udsclientsocket *udscs = uds_create_client(socketpath);
+  
+  //fprintf(stderr, "opening server socket with path \"%s\"\n", outsockpath);
+  //udsserversocket *udsss = uds_create_server(outsockpath);
+  //uds_start_server(udsss);
   
   double *lastnormvalue = assert_calloc(pd->solnum, sizeof(double));
   double *derivative1 = assert_malloc(sizeof(double) * pd->solnum);
@@ -47,32 +54,59 @@ int main(int argc, char *argv[]) {
   
   unsigned char buffer[GLOBALSEQPACKETSIZE];
   int bufferlength;
-  struct packet2byte *parsedinput = allocate2bytePacket(pd->solnum);
+  struct packet2byte *packet = allocate2bytePacket(pd->solnum);
+  
+  fprintf(stderr, "start reading data ...\n");
   while ( (bufferlength = uds_read(udscs, buffer, GLOBALSEQPACKETSIZE)) > 0) {
-    if (parse2bytePacket(buffer, bufferlength, parsedinput, 1, pd->solnum) != pd->solnum) {
+    if (parse2bytePacket(buffer, bufferlength, packet, 1, pd->solnum) != pd->solnum) {
       fprintf(stderr, "Received invalid packet.\n");
       continue;
     }
     
     for (int i = 0; i < pd->solnum; i++) {
       // normalize input value
-      normval = (double)parsedinput->values[i];
+      normval = (double)packet->values[i];
       normval -= pd->sols[i][IDX_MEAN];
       normval /= pd->sols[i][IDX_STD_DEVIATION];
       // first derivative
       d1 = normval - lastnormvalue[i];
       // second derivative
       d2 = derivative1[i] - d1;
+      
+      // find inversion
+      if ((derivative2[i] > 10 || d2 > 10) && derivative2[i] > d2) printf("x");
+      else printf("-");
+      //printf("%f,", derivative2[i]);
+      
       // store the values
       derivative1[i] = d1;
       derivative2[i] = d2;
       lastnormvalue[i] = normval;
     }
+    printf("\n");
+    
+    fflush(stdout);
+    
+    // TODO find pendulum
+    // TODO calculate angles
     
     //TODO write output
+    /*
+    // format dataset
+    bufferlength = format2bytePacket(buffer, BUFFERSIZES
+                                   , packet->timestamp
+                                   , [DATA], 2);
+    if (bufferlength <= 0) {
+      fprintf(stderr, "error while formatting packet, code: (%d)\n", bufferlength);
+      break;
+    } else
+      uds_send_toall(udsss, buffer, bufferlength);
+    */
   }
   
   fprintf(stderr, "end of data\n");
   uds_close_client(udscs);
+  //uds_stop_server(udsss);
+  unlink(outsockpath);
 }
 
