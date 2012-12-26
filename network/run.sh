54a5f42346c4ecef72ad233017cbd8143f2cd870
tychon <tychon4.2@googlemail.com>
2012-12-26 14:11:43 +0100

    tracker: read and process data

--- network/run.sh
+++ network/run.sh
@@ -8,7 +8,19 @@ if [ "$1" == "normalisation" ]; then
     ./replay.x -f -d -t -i values.csv -p data_pendulum -o rawsocket -r 100 &
     sleep 0.1
     echo -e "\nstarting normalisation ...\n\n"
-    ./normalisation.x -p data_pendulum -i rawsocket --samplenum 10 > data_normalisation
+    ./normalisation.x -p data_pendulum -i rawsocket --samplenum 100 > data_normalisation
+    echo -e "\nkilling replayer ... \n\n"
+    kill $!
+  fi
+fi
+
+if [ "$1" == "tracking" ]; then
+  if [ "$2" == "replay" ]; then
+    echo -e "\nstarting replayer ...\n\n"
+    ./replay.x -f -d -t -i values.csv -p data_pendulum -o rawsocket -r 1000 > /dev/null&
+    sleep 0.001
+    echo -e "\nstarting normalisation ...\n\n"
+    ./tracker.x -p data_pendulum -n data_normalisation -i rawsocket
     echo -e "\nkilling replayer ... \n\n"
     kill $!
   fi
--- network/tracker.c
+++ network/tracker.c
@@ -40,7 +40,37 @@ int main(int argc, char *argv[]) {
   printf("opening connection to server on \"%s\"\n", socketpath);
   udsclientsocket *udscs = uds_create_client(socketpath);
   
-  // TODO
+  double *lastnormvalue = assert_calloc(pd->solnum, sizeof(double));
+  double *derivative1 = assert_malloc(sizeof(double) * pd->solnum);
+  double *derivative2 = assert_malloc(sizeof(double) * pd->solnum);
+  double normval, d1, d2;
+  
+  unsigned char buffer[GLOBALSEQPACKETSIZE];
+  int bufferlength;
+  struct packet2byte *parsedinput = allocate2bytePacket(pd->solnum);
+  while ( (bufferlength = uds_read(udscs, buffer, GLOBALSEQPACKETSIZE)) > 0) {
+    if (parse2bytePacket(buffer, bufferlength, parsedinput, 1, pd->solnum) != pd->solnum) {
+      fprintf(stderr, "Received invalid packet.\n");
+      continue;
+    }
+    
+    for (int i = 0; i < pd->solnum; i++) {
+      // normalize input value
+      normval = (double)parsedinput->values[i];
+      normval -= pd->sols[i][IDX_MEAN];
+      normval /= pd->sols[i][IDX_STD_DEVIATION];
+      // first derivative
+      d1 = normval - lastnormvalue[i];
+      // second derivative
+      d2 = derivative1[i] - d1;
+      // store the values
+      derivative1[i] = d1;
+      derivative2[i] = d2;
+      lastnormvalue[i] = normval;
+    }
+    
+    //TODO write output
+  }
   
   fprintf(stderr, "end of data\n");
   uds_close_client(udscs);
