9e8a506a960ffafa35a90874df9e07ce9216bab8
tychon <tychon4.2@googlemail.com>
2013-02-12 19:08:57 +0100

    doc!

--- network/markov_chain.h
+++ network/markov_chain.h
@@ -4,23 +4,43 @@
 
 typedef struct markovchainmatrix {
   int statenum;
+  /// Holds the number of transitions recorded for every state.
+  /// Size: statenum*sizeof(int)
+  /// *Size: statenum*sizeof(int)
+  /// use: relations[statindex1][stateindex2] = number
   int **relations;
+  /// Holds the number of transitions done from every state.
+  /// Size: statenum * sizeof(int)
+  /// use: samplesperstate[stateindex] = number
   int *samplesperstate;
 } markovchainmatrix;
 
+/**
+ * Allocate all the memory for one markov chain.
+ */
 markovchainmatrix *allocateMarkovChain(int statenum);
 
 void markovchain_addsample(markovchainmatrix *matrix
                          , int stateindex_from
                          , int stateindex_dest);
 
+/**
+ * @returns the probability for the transition from one given state to
+ * another one.
+ */
 double markovchain_getprob(markovchainmatrix *matrix
                        , int stateindex_from
                        , int stateindex_dest);
 
+/**
+ * @returns the index of the most probable next state.
+ */
 int markovchain_getMostProbableNextState(markovchainmatrix *matrix
                                        , int stateindex_from);
 
+/**
+ * @returns the number of samples collected for one given state.
+ */
 int markovchain_getSamplesAt(markovchainmatrix *matrix
                            , int stateindex_from);
 
