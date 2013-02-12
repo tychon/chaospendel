38dd1b3b0bae33d6e2ea4363012dd3a755105c8a
Jann Horn <jannhorn@googlemail.com>
2013-02-12 19:31:02 +0100

    untested, but should work: use less RAM for pendulum state

--- network/markov_chain.c
+++ network/markov_chain.c
@@ -1,6 +1,7 @@
-
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 #include "common.h"
 
 #include "markov_chain.h"
@@ -10,9 +11,7 @@ markovchainmatrix *allocateMarkovChain(int statenum) {
   
   matrix->statenum = statenum;
   
-  matrix->relations = assert_malloc(statenum * sizeof(int*));
-  for (int i = 0; i < statenum; i++)
-    matrix->relations[i] = assert_calloc(statenum, sizeof(int));
+  matrix->relations = assert_calloc(statenum, sizeof(int*));
   
   matrix->samplesperstate = assert_calloc(statenum, sizeof(int));
   
@@ -22,6 +21,9 @@ markovchainmatrix *allocateMarkovChain(int statenum) {
 void markovchain_addsample(markovchainmatrix *matrix
                          , int stateindex_from
                          , int stateindex_dest) {
+  if (matrix->relations[stateindex_from] == NULL) {
+    matrix->relations[stateindex_from] = assert_calloc(matrix->statenum, sizeof(int));
+  }
   matrix->relations[stateindex_from][stateindex_dest] ++;
   matrix->samplesperstate[stateindex_from] ++;
 }
@@ -29,6 +31,7 @@ void markovchain_addsample(markovchainmatrix *matrix
 double markovchain_getprob(markovchainmatrix *matrix
                        , int stateindex_from
                        , int stateindex_dest) {
+  if (matrix->relations[stateindex_from] == NULL) return 0;
   return (double)matrix->relations[stateindex_from][stateindex_dest]
        / (double)matrix->samplesperstate[stateindex_from];
 }
@@ -38,7 +41,8 @@ int markovchain_getMostProbableNextState(markovchainmatrix *matrix
   int bestcount = 0;
   int bestindex = -1;
   for (int i = 0; i < matrix->statenum; i++) {
-    if (matrix->relations[stateindex_from][i] > bestcount) {
+    if (matrix->relations[stateindex_from] != NULL
+        && matrix->relations[stateindex_from][i] > bestcount) {
       bestcount = matrix->relations[stateindex_from][i];
       bestindex = i;
     }
@@ -67,7 +71,7 @@ void markovchain_printDOTLanguageToFile(markovchainmatrix *matrix, char *filepat
   // print all relations
   for (int from = 0; from < matrix->statenum; from++) {
     for (int dest = 0; dest < matrix->statenum; dest++) {
-      if (matrix->relations[from][dest] > 0) {
+      if (matrix->relations[from] != NULL && matrix->relations[from][dest] > 0) {
         fprintf(f, " Q%d -> Q%d [color = black, label=\"%lf\"];\n", from, dest, markovchain_getprob(matrix, from, dest));
       }
     }
@@ -84,13 +88,16 @@ void markovchain_writeDataFile(markovchainmatrix *matrix, char *filepath) {
     perror("opening file");
     exit(1);
   }
+  int *emptydata = calloc(matrix->statenum, sizeof(int));
   for (int i = 0; i < matrix->statenum; i++) {
     fprintf(stderr, ESCAPE_CLEARLINE"  step 1: (%2.1lf%%) writing line %d  ", (double)i / (double)matrix->statenum * 100.0, i+1);
-    if (fwrite(matrix->relations[i], sizeof(int), matrix->statenum, f) <= 0) {
+    int *data = (matrix->relations[i] == NULL) ? emptydata : matrix->relations[i];
+    if (fwrite(data, sizeof(int), matrix->statenum, f) <= 0) {
       perror("saving markov chain: writing binary data");
       exit(1);
     }
   }
+  free(emptydata);
   fprintf(stderr, "\n  step 2: writing additional line\n");
   if (fwrite(matrix->samplesperstate, sizeof(int), matrix->statenum, f) <= 0) {
     perror("saving markov chain: writing binary data");
@@ -101,6 +108,14 @@ void markovchain_writeDataFile(markovchainmatrix *matrix, char *filepath) {
   fprintf(stderr, "finished writing markov data file.\n");
 }
 
+static bool check_allnulls(void *data, size_t len) {
+  char *c = data, *e = data+len;
+  do {
+    if (*c != '\0') return false;
+  } while (++c != e);
+  return true;
+}
+
 void markovchain_readDataFile(char *filepath, markovchainmatrix *matrix) {
   FILE *f = fopen(filepath, "rb");
   if (! f) {
@@ -109,9 +124,16 @@ void markovchain_readDataFile(char *filepath, markovchainmatrix *matrix) {
   }
   for (int i = 0; i < matrix->statenum; i++) {
     fprintf(stderr, ESCAPE_CLEARLINE"  step 1: (%2.1lf%%) reading line %d  ", (double)i / (double)matrix->statenum * 100.0, i+1);
+    if (matrix->relations[i] == NULL) {
+      matrix->relations[i] = assert_calloc(matrix->statenum, sizeof(int));
+    }
     if (fread(matrix->relations[i], sizeof(int), matrix->statenum, f) <= 0) {
       perror("reading markov chain");
       exit(1);
+    }
+    if (check_allnulls(matrix->relations[i], sizeof(int)*matrix->statenum)) {
+      free(matrix->relations[i]);
+      matrix->relations[i] = NULL;
     }
   }
   fprintf(stderr, "\n  step 2: reading additional line\n");
