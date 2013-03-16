016bf86c7fe8495360a016a603b409d097564790
Jann Horn <jannhorn@googlemail.com>
2013-03-16 13:18:12 +0100

    lighttrail: townize variables, show loopings

--- network/lighttrail.c
+++ network/lighttrail.c
@@ -15,6 +15,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <linux/limits.h>
+#include <assert.h>
 
 #include "common.h"
 #include "projectreader.h"
@@ -25,11 +26,15 @@
 #include "integral.h"
 #include "../numerical/pendulum.h"
 
+static projectdata *pd;
+static shmsurface *sf;
+static char *datafilepath = "../numerical/out.bin";
+
 void toPendulumCartesian(double radius, double angle, double *x, double *y) {
   *x = sin(angle) * radius;
   *y = cos(angle) * radius;
 }
-void pstateToCartesianEnd(pstate *s, projectdata *pd, double *second_x, double *second_y) {
+void pstateToCartesianEnd(pstate *s, double *second_x, double *second_y) {
   double first_x, first_y;
   toPendulumCartesian(pd->l1, s->phi1, &first_x, &first_y);
   double second_x_rel, second_y_rel;
@@ -38,14 +43,20 @@ void pstateToCartesianEnd(pstate *s, projectdata *pd, double *second_x, double *
   *second_y = first_y+second_y_rel;
 }
 
-void drawPendulum(shmsurface *sf, projectdata *pd, pstate *states, int states_len, int whitebg) {
+static int whitebg = 0;
+
+void drawPendulum(pstate *states, int states_len) {
+  int left_loopings = 0, right_loopings = 0;
+  
   int green_color = COLOR_GREEN;
   int red_color = COLOR_RED;
+  int blue_color = COLOR_BLUE;
   // clear surface
   if (whitebg) {
     shmsurface_fill(sf, COLOR_WHITE);
     green_color = 0xff009600;
     red_color = 0xff960000;
+    blue_color = 0xff000096;
   } else shmsurface_fill(sf, COLOR_BLACK);
   
   // precompute scaling
@@ -58,56 +69,66 @@ void drawPendulum(shmsurface *sf, projectdata *pd, pstate *states, int states_le
   
   // draw data
   int last_xpos, last_ypos, xpos, ypos;
+  int last_section, cur_section;
   double xpos_, ypos_;
   for (int i = 0; i < states_len; i++) {
     pstate *s = &states[i];
-    pstateToCartesianEnd(s, pd, &xpos_, &ypos_);
+    
+    pstateToCartesianEnd(s, &xpos_, &ypos_);
     xpos = (int)(xpos_ * scale);
     ypos = (int)(ypos_ * scale);
     xpos += sf->width/2;
     ypos += sf->height/2;
     
+    cur_section = s->phi2/(M_PI/2);
+    cur_section = ((cur_section%4)+4)%4;
+    assert(cur_section >= 0 && cur_section < 4);
+    
     if (i != 0) {
       // the first point does not have a previous one
       // to which we could draw a line, so ignore it
       //fprintf(stderr, "(%d|%d)->(%d|%d)\n", last_xpos, last_ypos, xpos, ypos);
-      drawBresenhamLine(sf, last_xpos, last_ypos, xpos, ypos, green_color);
+      int color = green_color;
+      if (last_section == 1/*upper left*/ && cur_section == 2/*upper right*/) { color = red_color; right_loopings++; }
+      if (last_section == 2/*upper right*/ && cur_section == 1/*upper left*/) { color = blue_color; left_loopings++; }
+      drawBresenhamLine(sf, last_xpos, last_ypos, xpos, ypos, color);
     }
     
     last_xpos = xpos;
     last_ypos = ypos;
+    last_section = cur_section;
   }
+  
+  fprintf(stderr, "pendulum rendered. %d loopings to the left, %d loopings to the right.\n", left_loopings, right_loopings);
 }
 
-void run(char *datafpath, shmsurface *sf, projectdata *pd, int whitebg) {
+void run() {
   struct stat st;
-  if (stat(datafpath, &st)) {
-    fprintf(stderr, "can't stat \"%s\": %s\n", datafpath, strerror(errno));
+  if (stat(datafilepath, &st)) {
+    fprintf(stderr, "can't stat \"%s\": %s\n", datafilepath, strerror(errno));
     exit(1);
   }
   // file truncated? whatever.
   int states_len = st.st_size / sizeof(pstate);
-  int fd = open(datafpath, O_RDONLY);
+  int fd = open(datafilepath, O_RDONLY);
   if (fd == -1) {
-    fprintf(stderr, "can't open \"%s\": %s\n", datafpath, strerror(errno));
+    fprintf(stderr, "can't open \"%s\": %s\n", datafilepath, strerror(errno));
     exit(1);
   }
   void *states = mmap(NULL, states_len*sizeof(pstate), PROT_READ, MAP_PRIVATE, fd, 0);
   if (states == MAP_FAILED) {
-    fprintf(stderr, "can't mmap \"%s\n: %s\n", datafpath, strerror(errno));
+    fprintf(stderr, "can't mmap \"%s\n: %s\n", datafilepath, strerror(errno));
     exit(1);
   }
   close(fd);
   fprintf(stderr, "drawing pendulum with %d states on %s...\n", states_len, whitebg?"white":"black");
-  drawPendulum(sf, pd, states, states_len, whitebg);
+  drawPendulum(states, states_len);
   munmap(states, states_len*sizeof(pstate));
   flushSHMSurface(sf);
 }
 
 int main(int argc, char *argv[]) {
   char *pendulumdatapath = "data_pendulum";
-  char *datafilepath = "../numerical/out.bin";
-  int whitebg = 0;
   
   for (int i = 1; i < argc; i++) {
     if (argcmpass("--pendulum|-p", argc, argv, &i, &pendulumdatapath)) ;
@@ -118,13 +139,13 @@ int main(int argc, char *argv[]) {
     else fprintf(stderr, "warning: Unknown argument ignored: \"%s\"\n", argv[i]);
   }
   
-  projectdata *pd = assert_malloc(sizeof(projectdata));
+  pd = assert_malloc(sizeof(projectdata));
   
   fprintf(stderr, "reading pendulum data from \"%s\" ...\n", pendulumdatapath);
   readPendulumData(pd, pendulumdatapath);
   
   // x11 things
-  shmsurface *surface = createSHMSurface(100, 100, 500, 500);
+  sf = createSHMSurface(100, 100, 500, 500);
   
   int in_fd = inotify_init();
   if (in_fd == -1) {
@@ -137,7 +158,7 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
   
-  run(datafilepath, surface, pd, whitebg);
+  run();
   
   while (1) {
     size_t evt_size = sizeof(struct inotify_event)+NAME_MAX+1;
@@ -153,7 +174,7 @@ int main(int argc, char *argv[]) {
     }
     // we ignore the actual inotify event – there's only one thing it could
     // plausibly be, and if we're wrong, we just run one more iteration
-    run(datafilepath, surface, pd, whitebg);
+    run();
   }
 }
 
