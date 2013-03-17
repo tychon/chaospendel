5b36cf663663368cda2f0c800d39e6414573f733
tychon <tychon4.2@googlemail.com>
2013-03-17 13:30:55 +0100

    convert ncsv to pgm file

---
+++ numerical/ncsvtopgm.c
@@ -0,0 +1,114 @@
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "common.h"
+
+typedef enum {
+  ALL, ROWS, COLUMNS
+} autoscale;
+
+int main(int argc, char *argv[]) {
+  char *inputpath = NULL
+     , *outputpath = NULL;
+  int columns = 1
+    , pgmmaxval = 255;
+  bool mirror = false
+     , binary = false;
+  autoscale ascale = ALL;
+  
+  for (int i = 1; i < argc; i++) {
+    if      (argcmpass("--inputpath|-i", argc, argv, &i, &inputpath));
+    else if (argcmpass("--outputpath|-o", argc, argv, &i, &outputpath));
+    else if (argcmpassint("--columns|-c", argc, argv, &i, &columns));
+    else if (! strcmp("--mirror", argv[i])) mirror = true;
+    else if (! strcmp("--scalerows", argv[i])) ascale = ROWS;
+    else if (! strcmp("--scalecols", argv[i])) ascale = COLUMNS;
+    else if (! strcmp("--binary", argv[i])) binary = true;
+    else {
+      fprintf(stderr, "Unknown argument: %s\n", argv[i]);
+      exit(1);
+    }
+  }
+  
+  if (binary) pgmmaxval = 255;
+  
+  //// load csv file ////
+  int allocated_rows = 100;
+  double **data = assert_malloc(allocated_rows * sizeof(double*));
+  int rows = 0; // count rows
+  
+  FILE *inpf = inputpath ? fopen(inputpath, "r") : stdin;
+  if (! inpf) {
+    perror("Could not open input file");
+    exit(1);
+  }
+  double numbers[columns];
+  for (;;) {
+    if (feof(inpf)) break;
+    
+    if (rows == allocated_rows) {
+      allocated_rows += 1000;
+      data = assert_realloc(data, allocated_rows * sizeof(double*));
+    }
+    
+    if (rows ++, ! parseNCSVLine(inpf, columns, numbers) < 0) {
+      fprintf(stderr, "ERROR: Bad CSV file! line %d\n", rows);
+      exit(1);
+    }
+    data[rows-1] = assert_malloc(columns * sizeof(double));
+    memcpy(data[rows-1], numbers, columns * sizeof(double));
+  }
+  
+  if (inputpath) fclose(inpf);
+  
+  //// mirror and scale ////
+  
+  if (ascale == ALL) {
+    double maxval = 0;
+    for (int row = 0; row < rows; row ++) {
+      for (int col = 0; col < columns; col ++) {
+        if (data[row][col] < 0) {
+          fprintf(stderr, "Invalid number smaller zero in row %d col %d: %f\n", row+1, col+1, data[row][col]);
+          exit(1);
+        }
+        if (data[row][col] > maxval) maxval = data[row][col];
+      }
+    }
+    for (int row = 0; row < rows; row ++) {
+      for (int col = 0; col < columns; col ++) {
+        data[row][col] = data[row][col] / maxval * pgmmaxval;
+      }
+    }
+  }
+  else ; //TODO scale rows / cols
+  
+  
+  if (mirror) {
+    //TODO mirror
+  }
+  
+  //// output ////
+  FILE *outf = outputpath ? fopen(outputpath, "w+") : stdout;
+  if (! outf) {
+    perror("Could not open output file");
+    exit(1);
+  }
+  
+  if (binary) fprintf(outf, "P5\n%d %d\n%d\n", columns, rows, pgmmaxval);
+  else fprintf(outf, "P2\n%d %d\n%d\n", columns, rows, pgmmaxval);
+  
+  for (int row = 0; row < rows; row ++) {
+    for (int col = 0; col < columns-1; col ++) {
+      if (binary) fwrite(&data[row][col], 1, sizeof(unsigned char), outf);
+      else fprintf(outf, "%d,", (int)data[row][col]);
+    }
+    if (binary) fwrite(&data[row][columns-1], 1, sizeof(unsigned char), outf);
+    else fprintf(outf, "%d\n", (int)data[row][columns-1]);
+  }
+  
+  if (outputpath) fclose(outf);
+}
+
