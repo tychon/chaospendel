07d96a4e13e27ce0cf0ce21f9eddbd2a64cd8ca2
tychon <tychon4.2@googlemail.com>
2013-03-21 21:39:25 +0100

    allow more options

--- numerical/phases.sh
+++ numerical/phases.sh
@@ -35,13 +35,13 @@ for X in $(seq 0 $STEPS); do
   IMAGEFILE=$DIRECTORY"/out$X.png"
   TIMAGEFILE=$DIRECTORY"/out""$X""_torus.png"
   
-  if [[ "$1" == "simulations" ]]; then
+  if [ "$1" = "simulations" ] || [ "$1" = "all" ]; then
     # simulation
     MAINARGS="--time $TIME --outstep $OUTSTEP --phi1 $PHI1 --phi2 $PHI2"
     ./pendulum.x $MAINARGS > $DATAFILE 2> /dev/null
   fi
   
-  if [[ "$1" == "plots" ]]; then
+  if [ "$1" = "plots" ] || [ "$1" = "all" ]; then
     octave --eval "A=csvread(\"$DATAFILE\");
       plot3(A(:,1),A(:,2),A(:,4));
       title(\"phi1_0=$PHI1, phi2_0=$PHI2\");
@@ -52,7 +52,7 @@ for X in $(seq 0 $STEPS); do
       print -dpng \"$IMAGEFILE\";" > /dev/null
   fi
   
-  if [[ "$1" == "tori" ]]; then
+  if [ "$1" = "tori" ] || [ "$1" = "all" ]; then
     octave --eval "source torus.m;
       A = csvread(\"$DATAFILE\");
       plotTorus(periodic(A(:,1).*2.7,-pi,-pi,pi), periodic(A(:,2),-pi,-pi,pi), periodic(A(:,4),-0.1,-0.1,0.1), 10, 6);
