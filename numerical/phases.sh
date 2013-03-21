dd4127bc62c8ec169c58b8987b24d4d58193c210
tychon <tychon4.2@googlemail.com>
2013-03-21 21:23:30 +0100

    support normal space and tori in phases.sh

--- numerical/phases.sh
+++ numerical/phases.sh
@@ -27,25 +27,39 @@ PHI1_STEP=$(math "($PHI1_START - $PHI1_END) / $STEPS.0")
 PHI2_STEP=$(math "($PHI2_START - $PHI2_END) / $STEPS.0")
 PHI1=$PHI1_START
 PHI2=$PHI2_START
+
 echo -e "step\tphi1\tphi2"
 for X in $(seq 0 $STEPS); do
   echo -e "$X/$STEPS\t$PHI1\t$PHI2"
   DATAFILE=$DIRECTORY"/out$X.csv"
   IMAGEFILE=$DIRECTORY"/out$X.png"
+  TIMAGEFILE=$DIRECTORY"/out""$X""_torus.png"
   
-  # simulation
-  MAINARGS="--time $TIME --outstep $OUTSTEP --phi1 $PHI1 --phi2 $PHI2"
-  ./pendulum.x $MAINARGS > $DATAFILE 2> /dev/null
+  if [[ "$1" == "simulations" ]]; then
+    # simulation
+    MAINARGS="--time $TIME --outstep $OUTSTEP --phi1 $PHI1 --phi2 $PHI2"
+    ./pendulum.x $MAINARGS > $DATAFILE 2> /dev/null
+  fi
   
-  octave --eval "source torus.m;
-    A = csvread(\"$DATAFILE\");
-    plotTorus(periodic(A(:,1).*2.7,-pi,-pi,pi), periodic(A(:,2),-pi,-pi,pi), periodic(A(:,4),-0.1,-0.1,0.1), 10, 6);
-    view(-37.5,50);
-    title(\"phi1_0=$PHI1, phi2_0=$PHI2\");
-    xlabel(\"phi1\");
-    ylabel(\"phi2\");
-    zlabel(\"p2\");
-    print -dpng \"$IMAGEFILE\";" > /dev/null
+  if [[ "$1" == "plots" ]]; then
+    octave --eval "A=csvread(\"$DATAFILE\");
+      plot3(A(:,1),A(:,2),A(:,4));
+      title(\"phi1_0=$PHI1, phi2_0=$PHI2\");
+      xlabel(\"phi1\");
+      ylabel(\"phi2\");
+      zlabel(\"p2\");
+      axis(\"square\");
+      print -dpng \"$IMAGEFILE\";" > /dev/null
+  fi
+  
+  if [[ "$1" == "tori" ]]; then
+    octave --eval "source torus.m;
+      A = csvread(\"$DATAFILE\");
+      plotTorus(periodic(A(:,1).*2.7,-pi,-pi,pi), periodic(A(:,2),-pi,-pi,pi), periodic(A(:,4),-0.1,-0.1,0.1), 10, 6);
+      view(-37.5,50);
+      title(\"phi1_0=$PHI1, phi2_0=$PHI2\");
+      print -dpng \"$TIMAGEFILE\";" > /dev/null
+  fi
   
   # Add up angles for next step
   PHI1=$(math "$PHI1 - $PHI1_STEP")
