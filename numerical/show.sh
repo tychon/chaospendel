921f69654fd6cc3fbd8debdcfaa90a5c42a00653
tychon <tychon4.2@googlemail.com>
2013-05-28 22:59:54 +0200

    adjust makefile and get show.sh working

--- numerical/show.sh
+++ numerical/show.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source easy_bash.sh
+
 make pendulum.x fourier.x ncsvtopgm.x
 if [[ $? != 0 ]]; then
   echo "makefile error"
@@ -8,8 +10,17 @@ fi
 
 ./pendulum.x > out.csv 2> out.info
 
+FWINDOW=1000
 echo "l2=0.19" >> out.info
+echo "fourier1_window=$FWINDOW" >> out.info
+echo "fourier1_pgm_scaling=256" >> out.info
+echo "fourier2_window=$FWINDOW" >> out.info
+echo "fourier2_pgm_scaling=256" >> out.info
+FREQN=`math "$FWINDOW / 2 + 1"`
+echo $FREQN
 
-./fourier.x -c 1 < out.csv > out.fourier1.csv
-./fourier.x -c 1 < out.csv > out.fourier2.csv
+./fourier.x -c 1 -w $FWINDOW < out.csv > out.fourier1.csv
+./ncsvtopgm.x -c $FREQN -i out.fourier1.csv -o out.pend1.pgm # --maximum --logarithmic --scalerows
+./fourier.x -c 1 -w $FWINDOW < out.csv > out.fourier2.csv
+./ncsvtopgm.x -c $FREQN -i out.fourier2.csv -o out.pend2.pgm
 
